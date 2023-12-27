5973de64274df8fb9def05cc71b669e4178febbf
alexanderpalvanov <alex.p@apiiro.com>
2023-12-27 15:15:44 +0200

    wip

--- Octokit/Models/Response/Copilot/CopilotSeat.cs
+++ Octokit/Models/Response/Copilot/CopilotSeat.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace Octokit.Copilot
 {
+    [ExcludeFromCtorWithAllPropertiesConventionTest(nameof(Type))]
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class CopilotSeat
     {
         public CopilotSeat() {}
@@ -18,5 +22,9 @@ namespace Octokit.Copilot
         public DateTime UpdatedAt { get; private set; }
 
         public User Assignee { get; private set; }
+        
+        internal string DebuggerDisplay =>
+            string.Format(CultureInfo.InvariantCulture,
+                "User: Id: {0} Login: {1}", Assignee.Id, Assignee.Login);
     }
 }
--- Octokit/Models/Response/Copilot/CopilotSeatsResponse.cs
+++ Octokit/Models/Response/Copilot/CopilotSeatsResponse.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace Octokit.Copilot
 {
     /// <summary>
     /// Represents a Copilot seats response on GitHub.
     /// </summary>
+    [ExcludeFromCtorWithAllPropertiesConventionTest(nameof(Type))]
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class CopilotSeatsResponse
     {
         public CopilotSeatsResponse()
@@ -20,5 +25,9 @@ namespace Octokit.Copilot
         public int TotalSeats { get; private set;}
 
         public IReadOnlyList<CopilotSeat> Seats { get; private set;}
+        
+        internal string DebuggerDisplay =>
+            string.Format(CultureInfo.InvariantCulture,
+                "TotalSeats: {0}", TotalSeats);
     }
 }
